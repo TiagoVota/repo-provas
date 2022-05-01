7a2d86d7762e747e766a01bfb283e20a4b22fc1b
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2022-05-01 16:53:39 -0300

    fix: remake logout functionality

--- src/services/api.auth.js
+++ src/services/api.auth.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-import { makeConfig } from '../helpers/configHelper'
-
 import BASE_URL from './baseUrl'
 
 
@@ -21,13 +19,7 @@ const postSignUp = ({ name, email, password }) => {
 }
 
 
-const makeLogout = ({ token }) => {
-	return axios.delete(`${AUTH_URL}/logout`, makeConfig(token))
-}
-
-
 export {
 	postLogin,
 	postSignUp,
-	makeLogout,
 }
